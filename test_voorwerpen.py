5ef62660233769313275284cc307768f7b43a530
Geert <geertennema@gmail.com>
2019-09-25 14:19:32 +0200

    add line 13 en 14

--- test_voorwerpen.py
+++ test_voorwerpen.py
@@ -8,3 +8,7 @@ class voorwerpenTest(unittest.TestCase):
    def test_name(self):
        voorwerp = Voorwerp("speler") #omdat het een test is
        self.assertEqual(voorwerp.naam, "speler")
+
+
+if __name__ == '__main__':
+    unittest.main()
